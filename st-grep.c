37f2851ce2fd510b076ebe069f0429f532d9e93e
Marcin Ślusarz <marcin.slusarz@gmail.com>
2019-04-25 21:57:28 +0200

    Revert "add some WIP xml parsing code"
    This reverts commit 653b9fb803f5cede7d5263201d732a1f486b44e0.

--- st-grep.c
+++ st-grep.c
@@ -34,8 +34,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <libxml/xmlreader.h>
-
 enum outformat {FMT_TEXT, FMT_XML, FMT_JSON};
 
 static const struct option long_options[] = {
@@ -53,79 +51,6 @@ usage(void)
 	printf("      --st-format=xml/json/text\n");
 	printf("      --help\n");
 	printf("      --version\n");
-}
-
-/*
- * type == 'reg'
- * owner_name != 'name'
- * int(size) >= 1024
- * timespec(mtime) > timespec_from_datetime('2019-01-01 01:02:03.012345')
- * int(mode) & 0600 == 0600
- * bool(owner_read) != bool(group_read)
- * name[0, 2] == 'st'
- * name[-2, 2] == 'ls'
- */
-enum value_op {none, to_int, to_boolean, datetime_to_timespec, timespec_to_datetime};
-enum compare_op {EQ, NE, LE, LT, GE, GT};
-
-struct condition {
-	enum value_op attr_op;
-	const char *attr;
-
-	enum compare_op compare_op;
-
-	enum value_op value_op;
-	const char *value;
-};
-
-static void
-processNode(xmlTextReaderPtr reader) {
-	const xmlChar *name, *value;
-
-	name = xmlTextReaderConstName(reader);
-	if (name == NULL)
-		name = BAD_CAST "--";
-
-	int depth = xmlTextReaderDepth(reader);
-	if (depth == 0) {
-		if (strcmp(name, "st") != 0) {
-			fprintf(stderr,
-				"this is not st document (top-level='%s')\n",
-				name);
-			exit(2);
-		}
-
-		return;
-	}
-
-	if (depth != 1) {
-		fprintf(stderr, "this document is too deep\n");
-		exit(2);
-	}
-
-	int type = xmlTextReaderNodeType(reader);
-
-	if (type == XML_READER_TYPE_SIGNIFICANT_WHITESPACE)
-		return;
-	if (type != XML_READER_TYPE_ELEMENT) {
-		fprintf(stderr, "unhandled type: %d\n", type);
-		exit(2);
-	}
-
-	if (strcmp(name, "entry") != 0) {
-		fprintf(stderr, "unknown tag: '%s'\n", name);
-		exit(2);
-	}
-
-	xmlNodePtr node = xmlTextReaderCurrentNode(reader);
-	xmlAttr *attr = node->properties;
-	while (attr) {
-		xmlChar* value = xmlNodeListGetString(node->doc, attr->children, 1);
-		printf ("A %s: %s\n",attr->name, value);
-		xmlFree(value);
-
-		attr = attr->next;
-	}
 }
 
 int
@@ -172,30 +97,9 @@ main(int argc, char *argv[])
 		}
 	}
 
-	for (int i = optind; i < argc; ++i) {
-
-	}
-
 	if (format == FMT_JSON) {
 		fprintf(stderr, "json format not implemented yet\n");
 		return 2;
-	}
-
-	xmlTextReader *reader = xmlReaderForFd(0, NULL, NULL, 0);
-	if (!reader)
-		abort();
-
-	int ret;
-	do {
-		ret = xmlTextReaderRead(reader);
-		if (ret == 1)
-			processNode(reader);
-	} while (ret == 1);
-
-	xmlFreeTextReader(reader);
-
-	if (ret != 0) {
-		fprintf(stderr, "failed to parse\n");
 	}
 
 	return 0;
