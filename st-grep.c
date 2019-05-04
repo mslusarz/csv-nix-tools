224d84d9d1f61385eca874836c5a6487f77c5845
Marcin Ślusarz <marcin.slusarz@gmail.com>
2019-05-04 17:38:25 +0200

    first working version of csv parsing and arg parsing in grep command

--- st-grep.c
+++ st-grep.c
@@ -31,7 +31,10 @@
  */
 
 #include <getopt.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 static const struct option long_options[] = {
@@ -45,8 +48,73 @@ usage(void)
 {
 	printf("Usage: st-grep [OPTION]...\n");
 	printf("Options:\n");
+	printf("  -e column=value\n");
+	printf("  -v\n");
 	printf("      --help\n");
 	printf("      --version\n");
+}
+
+struct header {
+	char *name;
+	char *type;
+};
+
+static struct header
+parse_header(const char *start, const char *end)
+{
+	// XXX change format
+	const char *p = strchr(start, '(');
+	if (!p || p >= end) {
+		fprintf(stderr, "one of the columns does not have type name\n");
+		exit(2);
+	}
+
+	struct header h;
+	// XXX don't alloc
+	h.name = strndup(start, (uintptr_t)p - (uintptr_t)start);
+	if (!h.name) {
+		perror("strndup");
+		exit(2);
+	}
+
+	h.type = strndup(p + 1, (uintptr_t)end - 1 - (uintptr_t)p - 1);
+	if (!h.type) {
+		perror("strndup");
+		exit(2);
+	}
+
+	return h;
+}
+
+static void
+add_header(struct header **headers, size_t *nheaders,
+		const struct header *header)
+{
+	(*nheaders)++;
+
+	*headers = realloc(*headers, (*nheaders) * sizeof((*headers)[0]));
+	if (!*headers) {
+		perror("realloc");
+		exit(2);
+	}
+
+	memcpy(&(*headers)[*nheaders - 1], header, sizeof(*header));
+}
+
+struct data_pos {
+	size_t start;
+	size_t len;
+};
+
+static void
+yield_row(const char *buf, struct data_pos *positions, struct header *headers,
+		size_t nheaders)
+{
+	for (size_t i = 0; i < nheaders; ++i) {
+		printf("column %ld: '", i);
+		fwrite(&buf[positions[i].start], 1, positions[i].len, stdout);
+		printf("' end of column %ld %s\n", i, headers[i].name);
+	}
 }
 
 int
@@ -54,10 +122,63 @@ main(int argc, char *argv[])
 {
 	int opt;
 	int longindex;
+	int invert = 0;
+	struct condition {
+		char *column;
+		char *value;
+		size_t col_num;
+	};
 
-	while ((opt = getopt_long(argc, argv, "", long_options,
+	struct condition *conditions = NULL;
+	size_t nconditions = 0;
+
+	while ((opt = getopt_long(argc, argv, "e:v", long_options,
 			&longindex)) != -1) {
 		switch (opt) {
+			case 'e': {
+				const char *eq = strchr(optarg, '=');
+				if (!eq) {
+					fprintf(stderr, "incorrect -e syntax\n");
+					exit(2);
+				}
+				struct condition cond;
+				cond.column = strndup(optarg, (uintptr_t)eq -
+						(uintptr_t)optarg);
+				if (!cond.column) {
+					perror("strndup");
+					exit(2);
+				}
+
+				const char *start;
+				size_t len;
+				if (eq[1] == '\"') {
+					start = eq + 2;
+					len = strlen(start) - 1;
+				} else {
+					start = eq + 1;
+					len = strlen(start);
+				}
+				cond.value = strndup(start, len);
+				if (!cond.value) {
+					perror("strndup");
+					exit(2);
+				}
+
+				nconditions++;
+				conditions = realloc(conditions, nconditions *
+						sizeof(conditions[0]));
+				if (!conditions) {
+					perror("realloc");
+					exit(2);
+				}
+				memcpy(&conditions[nconditions - 1], &cond,
+						sizeof(cond));
+
+				break;
+			}
+			case 'v':
+				invert = 1;
+				break;
 			case 'V':
 				printf("git\n");
 				return 0;
@@ -75,6 +196,191 @@ main(int argc, char *argv[])
 				usage();
 				return 2;
 		}
+	}
+
+//	for (size_t i = 0; i < nconditions; ++i) {
+//		printf("'%s' = '%s'\n", conditions[i].column,
+//				conditions[i].value);
+//	}
+
+	char *line = NULL;
+	size_t buf_size = 0;
+	ssize_t line_len = getline(&line, &buf_size, stdin);
+	if (line_len < 1) {
+		perror("getline");
+		exit(2);
+	}
+
+	struct header *headers = NULL;
+	size_t nheaders = 0;
+
+	const char *start = line;
+	const char *comma;
+	do {
+		comma = strchr(start, ',');
+		if (comma) {
+			struct header h = parse_header(start, comma);
+
+			add_header(&headers, &nheaders, &h);
+
+			start = comma + 1;
+		}
+	} while (comma);
+
+	const char *nl = strchr(start, '\n');
+	if (!nl) {
+		fprintf(stderr, "corrupted input\n");
+		exit(2);
+	}
+
+	struct header h = parse_header(start, nl);
+	add_header(&headers, &nheaders, &h);
+
+	for (size_t i = 0; i < nconditions; ++i) {
+		bool found = false;
+		for (size_t j = 0; j < nheaders; ++j) {
+			found = strcmp(conditions[i].column, headers[j].name)
+					== 0;
+			if (found) {
+				conditions[i].col_num = j;
+				break;
+			}
+		}
+
+		if (!found) {
+			fprintf(stderr, "column '%s' not found in input\n",
+					conditions[i].column);
+			exit(2);
+		}
+	}
+
+	// XXX use header info
+	printf("%s", line);
+
+	free(line);
+
+//	for (size_t i = 0; i < nheaders; ++i)
+//		printf("'%s' '%s'\n", headers[i].name, headers[i].type);
+
+	struct data_pos *positions = malloc(nheaders * sizeof(positions[0]));
+	if (!positions) {
+		perror("malloc");
+		exit(2);
+	}
+
+
+	int eof = 0;
+	size_t buflen = 0;
+	char *buf = NULL;
+	size_t ready = 0;
+
+	int column = 0;
+	bool in_quoted_string = false;
+	bool last_char_was_quot = false;
+	positions[0].start = 0;
+	size_t start_off = 0;
+
+	while (1) {
+		size_t i = start_off;
+		while (i < ready) {
+			if (last_char_was_quot) {
+				if (buf[i] == '"') {
+					// this character was escaped
+
+					// so switch back to in_quot_string logic
+					last_char_was_quot = false;
+
+					// and continue from the next character
+					i++;
+					continue;
+				}
+
+				// last " was end of quoted string
+
+				// so reset quoting logic
+				last_char_was_quot = false;
+				in_quoted_string = false;
+
+				// and continue from the *same* character
+				continue;
+			} else if (in_quoted_string) {
+				if (buf[i] == '"')
+					last_char_was_quot = true;
+
+				i++;
+				continue;
+			} else {
+				if (buf[i] == ',' || buf[i] == '\n') {
+					// end of non-quoted column
+					positions[column].len = i - positions[column].start;
+					column++;
+					if (column == nheaders) {
+						yield_row(buf, positions,
+							headers, nheaders);
+
+						i++;
+						memmove(&buf[0], &buf[i], ready - i);
+						ready -= i;
+						i = 0;
+						column = 0;
+						continue;
+					}
+
+					// move on to the next column
+					i++;
+					positions[column].start = i;
+					continue;
+				} else if (buf[i] == '"') {
+					// if we are not at the beginning of
+					// a column, then the stream is corrupted
+					if (i != positions[column].start)
+						abort(); // XXX
+
+					// switch to quoted string logic
+					in_quoted_string = true;
+
+					// and continue from the next character
+					i++;
+					continue;
+				} else {
+					// we are in the middle of a column
+					i++;
+					continue;
+				}
+			}
+		}
+
+		if (eof)
+			break;
+
+		if (ready == buflen) {
+			buflen += 1024;
+			buf = realloc(buf, buflen);
+			if (!buf)
+				abort();
+		}
+
+		size_t nmemb = buflen - ready;
+		/*
+		 * Don't ask for too much, otherwise memove after each row
+		 * becomes a perf problem. TODO: figure out how to fix this
+		 * without impacting code readability.
+		 */
+		if (nmemb > 100)
+			nmemb = 100;
+
+		size_t readin = fread(&buf[ready], 1, nmemb, stdin);
+		if (readin < nmemb) {
+			if (feof(stdin)) {
+				eof = 1;
+			} else {
+				perror("fread");
+				exit(2);
+			}
+		}
+
+		start_off = ready;
+		ready += readin;
 	}
 
 	return 0;
