0b2bb998127ccca32b5e3843a8402ef31c6d3240
Marcin Ślusarz <marcin.slusarz@gmail.com>
2019-12-19 00:57:29 +0100

    group-members: port to merge_utils framework

--- src/group-members.c
+++ src/group-members.c
@@ -35,15 +35,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "merge_utils.h"
 #include "usr-grp.h"
 #include "utils.h"
 
 static const struct option opts[] = {
-	{"fields",	required_argument,	NULL, 'f'},
-	{"show",	no_argument,		NULL, 's'},
-	{"version",	no_argument,		NULL, 'V'},
-	{"help",	no_argument,		NULL, 'h'},
-	{NULL,		0,			NULL, 0},
+	{"fields",		required_argument,	NULL, 'f'},
+	{"merge-with-stdin",	no_argument,		NULL, 'M'},
+	{"label",		required_argument,	NULL, 'L'},
+	{"show",		no_argument,		NULL, 's'},
+	{"version",		no_argument,		NULL, 'V'},
+	{"help",		no_argument,		NULL, 'h'},
+	{NULL,			0,			NULL, 0},
 };
 
 static void
@@ -52,6 +55,8 @@ usage(FILE *out)
 	fprintf(out, "Usage: csv-group-members [OPTION]...\n");
 	fprintf(out, "Options:\n");
 	fprintf(out, "  -f, --fields=name1[,name2...]\n");
+	fprintf(out, "  -M, --merge-with-stdin\n");
+	fprintf(out, "  -L, --label label\n");
 	fprintf(out, "  -s, --show\n");
 	fprintf(out, "      --help\n");
 	fprintf(out, "      --version\n");
@@ -137,6 +142,8 @@ main(int argc, char *argv[])
 	int opt;
 	char *cols = NULL;
 	bool show = false;
+	bool merge_with_stdin = false;
+	char *label = NULL;
 
 	struct column_info columns[] = {
 			{ true, 0, "group_name", TYPE_STRING, print_group_name },
@@ -145,10 +152,16 @@ main(int argc, char *argv[])
 
 	size_t ncolumns = ARRAY_SIZE(columns);
 
-	while ((opt = getopt_long(argc, argv, "f:s", opts, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "f:L:Ms", opts, NULL)) != -1) {
 		switch (opt) {
 			case 'f':
 				cols = xstrdup_nofail(optarg);
+				break;
+			case 'L':
+				label = strdup(optarg);
+				break;
+			case 'M':
+				merge_with_stdin = true;
 				break;
 			case 's':
 				show = true;
@@ -174,7 +187,11 @@ main(int argc, char *argv[])
 	if (show)
 		csv_show();
 
-	csvci_print_header(columns, ncolumns);
+	struct csvmu_ctx ctx;
+	ctx.label = label;
+	ctx.merge_with_stdin = merge_with_stdin;
+
+	csvmu_print_header(&ctx, "group_member", columns, ncolumns);
 
 	load_groups();
 	load_users();
@@ -186,10 +203,11 @@ main(int argc, char *argv[])
 		for (size_t j = 0; j < groups[i].nmembers; ++j) {
 			r.user_idx = j;
 
-			csvci_print_row(&r, columns, ncolumns);
+			csvmu_print_row(&ctx, &r, columns, ncolumns);
 		}
 	}
 
+	free(ctx.label);
 	free_users();
 	free_groups();
 
