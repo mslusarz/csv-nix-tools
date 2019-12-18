3985ae71d0519bf2ba2e52ea5147e5add0d3d1f1
Marcin Ślusarz <marcin.slusarz@gmail.com>
2019-12-18 22:46:01 +0100

    groups: drop user_ids and user_names columns
    there's now a separate tool for this information

--- src/groups.c
+++ src/groups.c
@@ -58,61 +58,6 @@ usage(FILE *out)
 }
 
 static void
-merge_users_into_groups(void)
-{
-	for (size_t i = 0; i < ngroups; ++i) {
-		struct csv_group *g = &groups[i];
-		char **members = g->members;
-
-		g->users = xmalloc_nofail(g->nmembers, sizeof(g->users[0]));
-
-		size_t idx = 0;
-		for (size_t j = 0; j < g->nmembers; ++j) {
-			struct csv_user *u = find_user_by_name(members[j]);
-			if (u) {
-				g->users[idx++] = u;
-			} else {
-				fprintf(stderr,
-					"warning: group '%s' contains unknown user '%s'\n",
-					g->name, *members);
-			}
-		}
-	}
-
-	for (size_t i = 0; i < nusers; ++i) {
-		struct csv_user *u = &users[i];
-
-		struct csv_group *g = find_group_by_gid(u->gid);
-		if (!g) {
-			fprintf(stderr,
-				"warning: user '%s' belongs to unknown group %d\n",
-				u->name, u->gid);
-			continue;
-		}
-
-		bool found = false;
-		for (size_t j = 0; j < g->nmembers; ++j) {
-			if (g->users[j] == u) {
-				found = true;
-				break;
-			}
-		}
-
-		if (!found) {
-			g->members = xrealloc_nofail(g->members,
-					g->nmembers + 1,
-					sizeof(g->members[0]));
-			g->users = xrealloc_nofail(g->users,
-					g->nmembers + 1,
-					sizeof(g->users[0]));
-
-			g->members[g->nmembers] = xstrdup_nofail(u->name);
-			g->users[g->nmembers++] = u;
-		}
-	}
-}
-
-static void
 print_name(const void *p)
 {
 	const struct csv_group *g = p;
@@ -133,42 +78,6 @@ print_gid(const void *p)
 	printf("%u", g->gid);
 }
 
-static void
-print_user_ids(const void *p)
-{
-	const struct csv_group *g = p;
-	if (g->nmembers) {
-		if (g->nmembers > 1)
-			putc('"', stdout);
-
-		for (size_t j = 0; j < g->nmembers - 1; ++j)
-			printf("%u,", g->users[j]->uid);
-
-		printf("%u", g->users[g->nmembers - 1]->uid);
-
-		if (g->nmembers > 1)
-			putc('"', stdout);
-	}
-}
-
-static void
-print_user_names(const void *p)
-{
-	const struct csv_group *g = p;
-	if (g->nmembers) {
-		if (g->nmembers > 1)
-			putc('"', stdout);
-
-		for (size_t j = 0; j < g->nmembers - 1; ++j)
-			printf("%s,", g->users[j]->name);
-
-		printf("%s", g->users[g->nmembers - 1]->name);
-
-		if (g->nmembers > 1)
-			putc('"', stdout);
-	}
-}
-
 int
 main(int argc, char *argv[])
 {
@@ -180,8 +89,6 @@ main(int argc, char *argv[])
 			{ true, 0, "name",        TYPE_STRING, print_name },
 			{ true, 0, "passwd",      TYPE_STRING, print_passwd },
 			{ true, 0, "gid",         TYPE_INT,    print_gid },
-			{ true, 0, "user_ids",    TYPE_STRING, print_user_ids },
-			{ true, 0, "user_names",  TYPE_STRING, print_user_names },
 	};
 
 	size_t ncolumns = ARRAY_SIZE(columns);
@@ -218,19 +125,10 @@ main(int argc, char *argv[])
 	csvci_print_header(columns, ncolumns);
 
 	load_groups();
-	for (size_t i = 0; i < ncolumns; ++i) {
-		if (strcmp(columns[i].name, "user_ids") != 0 &&
-				strcmp(columns[i].name, "user_names") != 0)
-			continue;
-
-		load_users();
-		merge_users_into_groups();
-	}
 
 	for (size_t i = 0; i < ngroups; ++i)
 		csvci_print_row(&groups[i], columns, ncolumns);
 
-	free_users();
 	free_groups();
 
 	return 0;
